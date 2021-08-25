4e6e1180d69e866160bca24d17af200479ba75e7
CesarSP99 <cesarandres00@gmail.com>
2021-08-24 22:53:42 -0400

    Initial commit

---
+++ src/main/java/com/cesarsp/plugins/saddleremover/SaddleRemover.java
@@ -0,0 +1,17 @@
+package com.cesarsp.plugins.saddleremover;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SaddleRemover extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
