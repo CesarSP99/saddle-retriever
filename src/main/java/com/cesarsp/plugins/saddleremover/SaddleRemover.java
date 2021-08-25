792a4f503487c30c9c8a24ed747bc806a904c86b
CesarSP99 <cesarandres00@gmail.com>
2021-08-25 17:43:06 -0400

    Add logic to register event in Plugin class

--- src/main/java/com/cesarsp/plugins/saddleremover/SaddleRemover.java
+++ src/main/java/com/cesarsp/plugins/saddleremover/SaddleRemover.java
@@ -1,17 +1,32 @@
 package com.cesarsp.plugins.saddleremover;
 
+import com.cesarsp.plugins.saddleremover.Listeners.ShiftRightClickListener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 public final class SaddleRemover extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Logger logger = this.getLogger();
+        logger.info("Trying to initialize SaddleRemover...");
+        try{
+            getServer().getPluginManager().registerEvents(new ShiftRightClickListener(), this);
+            logger.info("SaddleRemover was initialized successfully! :D");
+        }
+        catch (Exception e){
+            logger.severe("An error has occurred");
+            logger.severe(e.toString());
+            getPluginLoader().disablePlugin(this);
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Logger logger = this.getLogger();
+        logger.info("Disabling Saddle Remover");
     }
 }
