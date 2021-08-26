f938ab034ccb47785807fd64522a3eb90734337f
CesarSP99 <cesarandres00@gmail.com>
2021-08-26 00:10:35 -0400

    Add support for permissions and custom messages

--- src/main/java/com/cesarsp/plugins/saddleremover/Listeners/ShiftRightClickListener.java
+++ src/main/java/com/cesarsp/plugins/saddleremover/Listeners/ShiftRightClickListener.java
@@ -1,22 +1,58 @@
 package com.cesarsp.plugins.saddleremover.Listeners;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 public class ShiftRightClickListener implements Listener {
+    private final FileConfiguration config;
+
+    public ShiftRightClickListener(FileConfiguration config) {
+        this.config = config;
+    }
+
     @EventHandler
     public void onEntityClick(PlayerInteractEntityEvent playerEntityInteraction) {
         Entity entity = playerEntityInteraction.getRightClicked();
         Player player = playerEntityInteraction.getPlayer();
         if ((entity instanceof Pig || entity instanceof Strider)
+                && playerEntityInteraction.getHand() == EquipmentSlot.OFF_HAND
                 && player.isSneaking()
                 && player.getInventory().getItemInMainHand().getType() == Material.AIR
                 && ((Steerable) entity).hasSaddle()) {
-            ((Steerable) entity).setSaddle(false);
+            tryToRemoveSaddle(player, (Steerable) entity);
+            //player.sendMessage("Fired");
+        }
+    }
+
+    private void tryToRemoveSaddle(Player player, Steerable steerableEntity) {
+        ConfigurationSection messages = config.getConfigurationSection("messages");
+        String pigNoPermission = messages != null ? messages.getString("no-permission-pig") : "No permission";
+        String striderNoPermission = messages != null ? messages.getString("no-permission-strider") : "No permission";
+        if (config.getBoolean("use-permissions")) {
+            if (steerableEntity instanceof Pig) {
+                if (player.hasPermission("saddleremover.pig")) {
+                    steerableEntity.setSaddle(false);
+                    player.getInventory().addItem(new ItemStack(Material.SADDLE, 1));
+                } else {
+                    player.sendMessage(pigNoPermission != null ? pigNoPermission : "No permission");
+                }
+            } else {
+                if (player.hasPermission("saddleremover.strider")) {
+                    steerableEntity.setSaddle(false);
+                    player.getInventory().addItem(new ItemStack(Material.SADDLE, 1));
+                } else {
+                    player.sendMessage(striderNoPermission != null ? striderNoPermission : "No permission");
+                }
+            }
+        } else {
+            steerableEntity.setSaddle(false);
             player.getInventory().addItem(new ItemStack(Material.SADDLE, 1));
         }
     }
